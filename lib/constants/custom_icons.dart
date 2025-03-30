42ea726e2b3962d051accf572079334f53e1ad61
M3kko <Brody@imaximus.com>
2025-03-30 15:44:43 -0500

    Updated pubspec

--- lib/constants/custom_icons.dart
+++ lib/constants/custom_icons.dart
@@ -1,16 +1,15 @@
-import 'package:flutter/widgets.dart';
+class AppIcons {
+  // Private constructor to prevent instantiation
+  AppIcons._();
 
-class PeakFitIcons {
-  PeakFitIcons._();
+  // Base path
+  static const String _basePath = 'assets/icons/';
 
-  static const String _fontFamily = 'PeakFitIcons';
-
-  // These codepoint values will come from your icon font generation tool
-  // Example values given here - you'll replace with actual values
-  static const IconData home = IconData(0xe800, fontFamily: _fontFamily);
-  static const IconData workout = IconData(0xe801, fontFamily: _fontFamily);
-  static const IconData progress = IconData(0xe802, fontFamily: _fontFamily);
-  static const IconData profile = IconData(0xe803, fontFamily: _fontFamily);
+  // Icon paths
+  static const String home = '${_basePath}home.svg';
+  static const String workout = '${_basePath}workout.svg';
+  static const String progress = '${_basePath}progress.svg';
+  static const String profile = '${_basePath}profile.svg';
 
 // Add more icons as needed
 }
